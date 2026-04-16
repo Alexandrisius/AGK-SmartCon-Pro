1a92e04808045ea0bbad1f98bf3eb7f98dc0196f
Александр Климович <klim95alex@yandex.by>
2026-04-16 12:03:55 +0300

    feat: build-and-deploy.bat now deploys to Revit 2023 + 2025

--- build-and-deploy.bat
+++ build-and-deploy.bat
@@ -5,53 +5,72 @@ echo SmartCon - Build and Deploy (Debug)
 echo ==========================================
 
 set "SLN_PATH=src\SmartCon.sln"
-set "CONFIG=Debug.R25"
-set "FRAMEWORK=net8.0-windows"
-set "REVIT_VERSION=2025"
 
 echo.
-echo [1/4] Building solution (%CONFIG%, %FRAMEWORK%)...
-dotnet build "%SLN_PATH%" -c %CONFIG% -f %FRAMEWORK% --verbosity minimal
+echo [1/6] Restoring NuGet packages...
+dotnet restore "%SLN_PATH%"
 if errorlevel 1 (
     echo.
-    echo [ERROR] Build failed!
+    echo [ERROR] Restore failed!
     pause
     exit /b 1
 )
-echo [OK] Build successful
+echo [OK] Restore successful
 
 echo.
-echo [2/4] Preparing deployment folder...
-set "ADDIN_FOLDER=%APPDATA%\Autodesk\Revit\Addins\%REVIT_VERSION%"
-set "SMARTCON_FOLDER=%ADDIN_FOLDER%\SmartCon"
-
-if not exist "%SMARTCON_FOLDER%" mkdir "%SMARTCON_FOLDER%"
-
-echo [OK] Folder ready: %SMARTCON_FOLDER%
-
-echo.
-echo [3/4] Deploying plugin files...
-copy /Y "src\SmartCon.App\bin\%CONFIG%\%FRAMEWORK%\*.dll" "%SMARTCON_FOLDER%\"
-copy /Y "src\SmartCon.App\Resources\SmartCon.addin" "%ADDIN_FOLDER%\"
-
-echo.
-echo [4/4] Deploying updater...
-set "UPDATER_FRAMEWORK=net8.0"
-copy /Y "src\SmartCon.Updater\bin\Debug\%UPDATER_FRAMEWORK%\SmartCon.Updater.exe" "%SMARTCON_FOLDER%\" 2>nul
-copy /Y "src\SmartCon.Updater\bin\Debug\%UPDATER_FRAMEWORK%\SmartCon.Updater.dll" "%SMARTCON_FOLDER%\" 2>nul
-if exist "src\SmartCon.Updater\bin\Debug\%UPDATER_FRAMEWORK%\SmartCon.Updater.deps.json" (
-    copy /Y "src\SmartCon.Updater\bin\Debug\%UPDATER_FRAMEWORK%\SmartCon.Updater.deps.json" "%SMARTCON_FOLDER%\" 2>nul
+echo [2/6] Building Revit 2025 (net8.0)...
+dotnet build "%SLN_PATH%" -c Debug -f net8.0-windows --no-restore --verbosity minimal
+if errorlevel 1 (
+    echo.
+    echo [ERROR] Build R25 failed!
+    pause
+    exit /b 1
 )
-if exist "src\SmartCon.Updater\bin\Debug\%UPDATER_FRAMEWORK%\SmartCon.Updater.runtimeconfig.json" (
-    copy /Y "src\SmartCon.Updater\bin\Debug\%UPDATER_FRAMEWORK%\SmartCon.Updater.runtimeconfig.json" "%SMARTCON_FOLDER%\" 2>nul
+echo [OK] R25 build successful
+
+echo.
+echo [3/6] Building Revit 2023 (net48)...
+dotnet build "%SLN_PATH%" -c Debug -f net48 --no-restore --verbosity minimal -p:RevitVersion=2023
+if errorlevel 1 (
+    echo.
+    echo [ERROR] Build R23 failed!
+    pause
+    exit /b 1
 )
+echo [OK] R23 build successful
+
+echo.
+echo [4/6] Deploying to Revit 2025...
+set "ADDIN_R25=%APPDATA%\Autodesk\Revit\Addins\2025"
+set "SMARTCON_R25=%APPDATA%\SmartCon\2025"
+if not exist "%SMARTCON_R25%" mkdir "%SMARTCON_R25%"
+copy /Y "src\SmartCon.App\bin\Debug\net8.0-windows\*.dll" "%SMARTCON_R25%\" >nul
+copy /Y "src\SmartCon.App\bin\Debug\net8.0-windows\SmartCon.App.deps.json" "%SMARTCON_R25%\" >nul 2>nul
+copy /Y "src\SmartCon.App\Resources\SmartCon-2025.addin" "%ADDIN_R25%\" >nul
+echo [OK] Revit 2025 -^> %SMARTCON_R25%
+
+echo.
+echo [5/6] Deploying to Revit 2023...
+set "ADDIN_R23=%APPDATA%\Autodesk\Revit\Addins\2023"
+set "SMARTCON_R23=%APPDATA%\SmartCon\2021-2023"
+if not exist "%SMARTCON_R23%" mkdir "%SMARTCON_R23%"
+copy /Y "src\SmartCon.App\bin\Debug\net48\*.dll" "%SMARTCON_R23%\" >nul
+copy /Y "src\SmartCon.App\Resources\SmartCon-2023.addin" "%ADDIN_R23%\" >nul
+echo [OK] Revit 2023 -^> %SMARTCON_R23%
+
+echo.
+echo [6/6] Deploying updater...
+set "UPDATER_SRC=src\SmartCon.Updater\bin\Debug\net8.0"
+if not exist "%APPDATA%\SmartCon" mkdir "%APPDATA%\SmartCon"
+copy /Y "%UPDATER_SRC%\SmartCon.Updater.exe" "%APPDATA%\SmartCon\" >nul 2>nul
+copy /Y "%UPDATER_SRC%\SmartCon.Updater.dll" "%APPDATA%\SmartCon\" >nul 2>nul
+copy /Y "%UPDATER_SRC%\SmartCon.Updater.deps.json" "%APPDATA%\SmartCon\" >nul 2>nul
+copy /Y "%UPDATER_SRC%\SmartCon.Updater.runtimeconfig.json" "%APPDATA%\SmartCon\" >nul 2>nul
+echo [OK] Updater -^> %APPDATA%\SmartCon\
 
 echo.
 echo ==========================================
-echo [SUCCESS] Deployed to Revit %REVIT_VERSION%
+echo [SUCCESS] Deployed to Revit 2023 + 2025
 echo ==========================================
-echo.
-echo Files deployed:
-dir /b "%SMARTCON_FOLDER%"
 echo.
 pause
